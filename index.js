7c07feffe7296315bdb82a732c75d583ff952382
Ben Mcleland <bmclela>
2020-01-03 16:13:05 -0500

    added 5 minute ping

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
-const bodyParser = require("body-parser");
-var CronJob = require("cron").CronJob;
-require("./models/Team");
-require("./models/Game");
-require("./models/Player");
-require("./models/Backup");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const bodyParser = require('body-parser');
+var CronJob = require('cron').CronJob;
+require('./models/Team');
+require('./models/Game');
+require('./models/Player');
+require('./models/Backup');
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
 
 const app = express();
 
 app.use(bodyParser.json());
 
-require("./routes/gameRoutes")(app);
-const resetAll = require("./logic/resetAll");
+require('./routes/gameRoutes')(app);
+const resetAll = require('./logic/resetAll');
 
 // Resets rankings every day
 // new CronJob(
@@ -33,12 +33,17 @@ const resetAll = require("./logic/resetAll");
 //   "America/Los_Angeles"
 // );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+var http = require('http');
+setInterval(function() {
+  http.get('http://foos-tracker.herokuapp.com');
+}, 300000); // every 5 minutes (300000)
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
